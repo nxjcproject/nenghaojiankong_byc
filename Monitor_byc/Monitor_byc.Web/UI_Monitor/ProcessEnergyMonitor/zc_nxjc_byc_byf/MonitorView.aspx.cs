6e8af1d5932fa59373810f09af55532dd4679d32
zcs0228 <zcs0228@yahoo.com>
2015-02-09 15:33:02 +0800

    添加文件夹，及Service程序

---
+++ Monitor_byc/Monitor_byc.Web/UI_Monitor/ProcessEnergyMonitor/zc_nxjc_byc_byf/MonitorView.aspx.cs
@@ -0,0 +1,30 @@
+﻿using Monitor_byc.Infrastructure.Configuration;
+using Monitor_byc.Service.ProcessEnergyMonitor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Monitor_byc.Web.UI_Monitor.ProcessEnergyMonitor.zc_nxjc_byc_byf
+{
+    public partial class MonitorView : System.Web.UI.Page
+    {
+        private static readonly string connString = ConnectionStringFactory.NXJCConnectionString;          //DCS连接字符串
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        [WebMethod]
+        public static SceneMonitor GetRealTimeData(string organizationId, string sceneName)
+        {
+            SceneMonitor result = new SceneMonitor();
+            result.time = DateTime.Now;
+            result.Name = sceneName;
+            return result;
+        }
+    }
+}
